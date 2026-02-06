183201f8d720c77daf88f73f62648cd3764a9eb2
thinhh0x1l <hoangthinhh18.deva@gmail.com>
2026-02-06 21:31:10 +0700

    update fontAwesomeIcon

--- blog-view/src/main.js
+++ blog-view/src/main.js
@@ -3,47 +3,14 @@ import { createApp } from 'vue'
 import { createPinia } from "pinia";
 import App from './App.vue'
 import router from 'router/index.js'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import {
-    faHome,
-    faTags,
-    faArchive,
-    faTag,
-    faInfoCircle,
-    faCalendarAlt,
-    faEye,
-    faPencilAlt,
-    faClock,
-    faBars,
-    faSearch,
-    faCopy,
-    faFolderOpen,
-    faLightbulb
-} from '@fortawesome/free-solid-svg-icons'
+
 import '@/util/dateTimeFormatUtils.js'
 import '@/assets/css/base.css'
 import '@/assets/css/badge.css'
 import '@/assets/css/typo.css'
 import {PrismPlugin} from '@/plugins/prism/prism.js'
 import PrimeVuePlugin from "@/plugins/primevueConfig/primePluginVue.js";
-
-library.add(
-    faHome,
-    faTags,
-    faArchive,
-    faTag,
-    faInfoCircle,
-    faCalendarAlt,
-    faEye,
-    faPencilAlt,
-    faClock,
-    faBars,
-    faSearch,
-    faCopy,
-    faLightbulb,
-    faFolderOpen
-)
+import FontAwesomeIcon from "@/plugins/fontAwesomeIcon.js";
 
 const app = createApp(App)
 const pinia = createPinia()
---
+++ blog-view/src/plugins/fontAwesomeIcon.js
@@ -0,0 +1,40 @@
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+    faHome,
+    faTags,
+    faArchive,
+    faTag,
+    faInfoCircle,
+    faCalendarAlt,
+    faEye,
+    faPencilAlt,
+    faClock,
+    faBars,
+    faSearch,
+    faCopy,
+    faFolderOpen,
+    faLightbulb,
+    faEnvelope,
+    faUser,
+} from '@fortawesome/free-solid-svg-icons'
+library.add(
+    faHome,
+    faTags,
+    faArchive,
+    faTag,
+    faInfoCircle,
+    faCalendarAlt,
+    faEye,
+    faPencilAlt,
+    faClock,
+    faBars,
+    faSearch,
+    faCopy,
+    faLightbulb,
+    faFolderOpen,
+    faEnvelope,
+    faUser,
+)
+
+export default FontAwesomeIcon
